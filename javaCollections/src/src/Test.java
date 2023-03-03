cc9095143e8c3928f56ee0bd1a6958d2ae7ccaa6
Bohdan <notpadumink01@gmail.com>
2023-03-03 09:03:10 +0200

    Learning the Lists

---
+++ javaCollections/src/src/Test.java
@@ -0,0 +1,17 @@
+package src;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Test {
+	public static void main(String[] args) {
+		MyLinkedList myLinkedList = new MyLinkedList();
+		
+		myLinkedList.add(2);
+		myLinkedList.add(3);
+		myLinkedList.add(5);
+		
+		System.out.println(myLinkedList);
+	}
+
+}
